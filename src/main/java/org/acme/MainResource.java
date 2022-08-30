303724867408626b9c1836d98f57ea151cebcea3
Jaime Gonzalez Aguilar <jaimegag@gmail.com>
2022-08-30 11:32:56 -0400

    First commit Quarkus hello world

---
+++ src/main/java/org/acme/MainResource.java
@@ -0,0 +1,16 @@
+package org.acme;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/main")
+public class MainResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String main() {
+        return "Hello from Quarkus RESTEasy Reactive app";
+    }
+}
